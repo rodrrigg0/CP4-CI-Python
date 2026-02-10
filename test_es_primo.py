840b17a2c2a4d08910d82a507359a99b87dfb795
rodrigo <rodrigobachillersanz@gmail.com>
2026-02-10 02:08:29 +0100

    Añado workflow de CI

--- test_es_primo.py
+++ test_es_primo.py
@@ -1,7 +1,7 @@
 from es_primo import es_primo
 
 def test_2_es_primo():
-    assert es_primo(2) is False
+    assert es_primo(2) is True
 
 def test_4_no_es_primo():
     assert es_primo(4) is False
